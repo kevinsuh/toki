96c8831594be8b66e78e2544eb74d4acb51ff263
kevinsuh <ksuh@dinnerlab.com>
2016-06-09 19:08:33 -0400

    Create first table with postgres

--- src/dev_slackbot.js
+++ src/dev_slackbot.js
@@ -28,17 +28,3 @@ bot.startRTM((err) => {
   }
 });
 
-/**
- * 		experimenting around with pg
- */
-
-// import pg from 'pg';
-// var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
-
-// var client = new pg.Client(connectionString);
-// client.connect();
-// var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
-// query.on('end', function() { client.end(); });
-
-
-
