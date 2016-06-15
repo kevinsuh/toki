510d8822fcc7ac5ccaf061b7cdbd3b646b072429
kevinsuh <ksuh@dinnerlab.com>
2016-06-15 16:09:05 -0400

    Update `IS_BACK` flow

--- src/bot/controllers/work_sessions/index.js
+++ src/bot/controllers/work_sessions/index.js
@@ -2,12 +2,14 @@ import os from 'os';
 import { wit } from '../index';
 
 import models from '../../../app/models';
+import moment from 'moment-timezone';
 
 import endWorkSessionController from './endWorkSession';
 import middleWorkSessionController from './middleWorkSession';
 import startWorKSessionController from './startWorKSession';
 
 import intentConfig from '../../lib/intents';
+import { convertToSingleTaskObjectArray, convertArrayToTaskListMessage } from '../../lib/messageHelpers';
 
 // base controller for work sessions
 export default function(controller) {
@@ -39,58 +41,89 @@ export default function(controller) {
 				include: [ models.SlackUser ]
 			})
 			.then((user) => {
-				bot.startConversation(message, (err, convo) => {
 
-					convo.isBack = {
-						SlackUserId,
-						isBackDecision: false // what user wants to do
-					}
+				// temporary fix to get tasks
+				var timeAgoForTasks = moment().subtract(14, 'hours').format("YYYY-MM-DD HH:mm:ss");
+				user.getDailyTasks({
+					where: [`"DailyTask"."createdAt" > ? AND "Task"."done" = ?`, timeAgoForTasks, false],
+					include: [ models.Task ],
+					order: `"DailyTask"."priority" ASC`
+				})
+				.then((dailyTasks) => {
 
-					var name = user.nickName || user.email;
+					dailyTasks = convertToSingleTaskObjectArray(dailyTasks, "daily");
+					var taskListMessage = convertArrayToTaskListMessage(dailyTasks);
 
-					convo.say(`Welcome back, ${name}!`);
-					convo.ask(`I can help you start another session or end the day :grin:`, (response, convo) => {
-						// should eventually contain logic for 5 hour start_day vs start_session
-						var { intentObject: { entities } } = response;
-						var { intent }                     = entities;
-						var intentValue                    = (intent && intent[0]) ? intent[0].value : null;
+					bot.startConversation(message, (err, convo) => {
 
-						if (intentValue) {
-							switch(intentValue) {
-								case intentConfig.START_SESSION:
-									convo.isBackDecision = intentConfig.START_SESSION;
-									convo.say(`Let's do this :thumbsup:`);
-									break;
-								case intentConfig.END_DAY:
-									convo.isBackDecision = intentConfig.END_DAY;
-									convo.say(`Let's review the day! :pencil:`);
-									break;
-								default:
-									convo.say(`Totally cool, just let me know when you're ready to do either of those things! :wave:`);
-									break;
-							}
+						convo.isBack = {
+							SlackUserId,
+							isBackDecision: false // what user wants to do
 						}
-						convo.next();
 
-					});
-					convo.on(`end`, (convo) => {
-						const { isBackDecision } = convo;
-						if (convo.status == 'completed') {
-							switch (isBackDecision) {
-								case intentConfig.START_SESSION:
-									console.log("\n\n\nSTART_SESSION!\n\n\n");
-									break;
-								case intentConfig.END_DAY:
-									console.log("\n\n\nEND_DAY\n\n\n");
-									break;
-								default:
-									break;
-							}
-						} else {
-							bot.reply(message, "Okay! Let me know when you want to start a session or day");
+						var name = user.nickName || user.email;
+
+						convo.say(`Welcome back, ${name}!`);
+						if (dailyTasks.length > 0) {
+						 convo.say(`Here are your priorities from our last time together:\n${taskListMessage}`);
 						}
+						var options = ["• start a work session with your most recent priorities", "• set new priorities for today - and we can move any of your most recent priorities into this new list, too!", "• end our day together"];
+						var optionsList = "";
+						options.forEach((option) => {
+							optionsList = `${optionsList}> ${option}\n`;
+						})
+						convo.ask(`What would you like to do now? I can help you with any of these things:\n${optionsList}`, (response, convo) => {
+							// should eventually contain logic for 5 hour start_day vs start_session
+							var { intentObject: { entities } } = response;
+							var { intent }                     = entities;
+							var intentValue                    = (intent && intent[0]) ? intent[0].value : null;
+
+							if (intentValue) {
+								switch(intentValue) {
+									case intentConfig.START_SESSION:
+										convo.isBackDecision = intentConfig.START_SESSION;
+										convo.say(`Love it. Let's kick off a new session :soccer:`);
+										break;
+									case intentConfig.END_DAY:
+										convo.isBackDecision = intentConfig.END_DAY;
+										convo.say(`It's about that time, isn't it?`);
+										break;
+									case intentConfig.VIEW_TASKS:
+										convo.say(`I agree - a fresh start seems like a great idea. Let's do it! :tangerine:`);
+										break;
+									default:
+										convo.say(`Totally cool, just let me know when you're ready to do either of those things! :wave:`);
+										break;
+								}
+							}
+							convo.next();
+
+						});
+						convo.on(`end`, (convo) => {
+							const { isBackDecision } = convo;
+							if (convo.status == 'completed') {
+								switch (isBackDecision) {
+									case intentConfig.START_SESSION:
+										console.log("\n\n\nSTART_SESSION!\n\n\n");
+										break;
+									case intentConfig.END_DAY:
+										console.log("\n\n\nEND_DAY\n\n\n");
+										break;
+									case intentConfig.VIEW_TASKS:
+									console.log("\n\n\nVIEW_TASKS\n\n\n");
+										break;
+									default:
+										break;
+								}
+							} else {
+								bot.reply(message, "Okay! Let me know when you want to start a session or day");
+							}
+						});
 					});
-				});
+
+				})
+
+				
 			});
 		}, 1000);
 		
--- src/bot/lib/intents.js
+++ src/bot/lib/intents.js
@@ -1,7 +1,8 @@
 const intentConfig =  {
 	WANT_BREAK: 'want_break',
 	START_SESSION: 'start_session',
-	END_DAY: 'end_day'
+	END_DAY: 'end_day',
+	VIEW_TASKS: 'view_tasks'
 }
 
 export default intentConfig;
