4ad9121c33f9a696e3f9b6c9b8dd80c8fcd5eba2
kevinsuh <kevinsuh34@gmail.com>
2016-07-09 16:51:54 -0400

    test commit

--- src/bot/controllers/misc/index.js
+++ src/bot/controllers/misc/index.js
@@ -480,7 +480,7 @@ function askUserToStartDay(response, convo) {
 				convo.next();
 			}
 		},
-		{
+		{ 
 			default: true,
 			callback: (response, convo) => {
 				convo.say(`Well, this is a bit embarrassing. Say \`start the day\` to keep moving forward so I can show you how I can help you work`);
