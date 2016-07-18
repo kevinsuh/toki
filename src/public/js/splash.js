dd830c4badabf0717cdbc611e619f821f2d0eab0
kevinsuh <kevinsuh34@gmail.com>
2016-07-17 21:58:44 -0400

    Start of displaying the full screen demo div using jQuery

--- src/public/js/splash.js
+++ src/public/js/splash.js
@@ -3,19 +3,34 @@
  */
 
 $(document).ready(function(){
-
-	var orange = `#FFA819`;
-	var blue   = `#3F76FF`;
 	
-	// hover to change color functionality
-	// $(".benefit-box").mouseenter(() => {
-	// 	$(this).animate({
-	// 		color: orange
-	// 	}, 500);
-	// }).mouseout(() => {
-	// 	$(this).animate({
-	// 		color: blue
-	// 	}, 500);
-	// })
+	// click to get appropriate gif
+	$(".benefit-box").click(function() {
+		var index = $(".benefit-box").index(this);
+
+		var demoDisplay = $("#demo-display");
+		demoDisplay.fadeIn(300);
+
+		switch (index) {
+			case 0:
+				// clear objectives, every day
+				break;
+			case 1:
+				// empower your self-awareness
+				break;
+			case 2:
+				// know what's next
+				break;
+			case 3:
+				// free up your attention
+				break;
+			alert($(".benefit-box").index(this));
+		}
+	});
+
+	$("#demo-display .circle").click(function() {
+		var demoDisplay = $("#demo-display");
+		demoDisplay.fadeOut(300);
+	});
 
 });
