8e04cbadd564b1181fef39731b534753b9d49cd2
kevinsuh <kevinsuh34@gmail.com>
2016-07-17 21:03:15 -0400

    Integrate first jQuery file into page

---
+++ build/public/js/splash.js
@@ -0,0 +1,8 @@
+"use strict";
+
+$(document).ready(function () {
+	$(".body-container").click(function () {
+		$(this).hide();
+	});
+});
+//# sourceMappingURL=splash.js.map
---
+++ src/public/js/splash.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	$(".body-container").click(function(){
+		$(this).hide();
+	});
+});
