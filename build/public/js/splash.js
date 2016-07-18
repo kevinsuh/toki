00942073ebc2bf833ad0726d0cf02d3e79a4e5f0
kevinsuh <kevinsuh34@gmail.com>
2016-07-17 23:32:38 -0400

    Display demo views and gifs functional

--- build/public/js/splash.js
+++ build/public/js/splash.js
@@ -10,7 +10,10 @@ $(document).ready(function () {
 	$(".benefit-box").click(function () {
 
 		var demoDisplay = $("#demo-display");
-		demoDisplay.fadeIn(300);
+		demoDisplay.fadeIn(300, function () {
+			var mainDisplay = $("#splash-main-display");
+			mainDisplay.fadeOut(300);
+		});
 
 		var index = $(".benefit-box").index(this);
 		updateDemoDivOnIndex(index);
@@ -23,6 +26,10 @@ $(document).ready(function () {
 	});
 
 	$("#demo-display .circle").click(function () {
+
+		var mainDisplay = $("#splash-main-display");
+		mainDisplay.show();
+
 		var demoDisplay = $("#demo-display");
 		demoDisplay.fadeOut(300);
 	});
@@ -35,6 +42,8 @@ function updateDemoDivOnIndex(index) {
 	var title = "Demo";
 	var leftIndex = 0;
 	var rightIndex = 1;
+	var imageSource = "clearobjectives.gif";
+	var imageAlt = "Clear Objectives";
 
 	switch (index) {
 		case 0:
@@ -42,24 +51,32 @@ function updateDemoDivOnIndex(index) {
 			title = "Clear objectives, every day";
 			leftIndex = 3;
 			rightIndex = 1;
+			imageSource = "clearobjectives.gif";
+			imageAlt = "Clear Objectives";
 			break;
 		case 1:
 			// empower your self-awareness
 			title = "Empower your self-awareness";
 			leftIndex = 0;
 			rightIndex = 2;
+			imageSource = "empowerselfaware.gif";
+			imageAlt = "Empower your self-awareness";
 			break;
 		case 2:
 			// know what's next
 			title = "Know what's next";
 			leftIndex = 1;
 			rightIndex = 3;
+			imageSource = "next.gif";
+			imageAlt = "Know what's next";
 			break;
 		case 3:
 			// free up your attention
 			title = "Free up your attention";
 			leftIndex = 2;
 			rightIndex = 0;
+			imageSource = "freeattention.gif";
+			imageAlt = "Free up your attention";
 			break;
 		default:
 			break;
@@ -67,9 +84,13 @@ function updateDemoDivOnIndex(index) {
 
 	var counter = index + 1;
 
+	imageSource = "/assets/gifs/" + imageSource;
+
 	demoDisplay.find(".title").text(title);
 	demoDisplay.find(".index-counter").text(counter);
 	demoDisplay.find(".button.left").data("index", leftIndex);
 	demoDisplay.find(".button.right").data("index", rightIndex);
+	demoDisplay.find(".gif").attr("src", imageSource);
+	demoDisplay.find(".gif").attr("alt", imageAlt);
 }
 //# sourceMappingURL=splash.js.map
--- src/public/js/splash.js
+++ src/public/js/splash.js
@@ -8,7 +8,10 @@ $(document).ready(function(){
 	$(".benefit-box").click(function() {
 
 		var demoDisplay = $("#demo-display");
-		demoDisplay.fadeIn(300);
+		demoDisplay.fadeIn(300, function() {
+			var mainDisplay = $("#splash-main-display");
+			mainDisplay.fadeOut(300);
+		});
 
 		var index = $(".benefit-box").index(this);
 		updateDemoDivOnIndex(index);
@@ -22,6 +25,10 @@ $(document).ready(function(){
 	}));
 
 	$("#demo-display .circle").click(function() {
+
+		var mainDisplay = $("#splash-main-display");
+		mainDisplay.show();
+
 		var demoDisplay = $("#demo-display");
 		demoDisplay.fadeOut(300);
 	});
@@ -32,34 +39,44 @@ function updateDemoDivOnIndex(index) {
 
 	var demoDisplay = $("#demo-display");
 
-	var title      = "Demo";
-	var leftIndex  = 0;
-	var rightIndex = 1;
+	var title       = "Demo";
+	var leftIndex   = 0;
+	var rightIndex  = 1;
+	var imageSource = "clearobjectives.gif";
+	var imageAlt    = "Clear Objectives";
 
 	switch (index) {
 		case 0:
 			// clear objectives, every day
-			title      = "Clear objectives, every day";
-			leftIndex  = 3;
-			rightIndex = 1;
+			title       = "Clear objectives, every day";
+			leftIndex   = 3;
+			rightIndex  = 1;
+			imageSource = "clearobjectives.gif";
+			imageAlt    = "Clear Objectives";
 			break;
 		case 1:
 			// empower your self-awareness
-			title = "Empower your self-awareness";
-			leftIndex  = 0;
-			rightIndex = 2;
+			title       = "Empower your self-awareness";
+			leftIndex   = 0;
+			rightIndex  = 2;
+			imageSource = "empowerselfaware.gif";
+			imageAlt    = "Empower your self-awareness";
 			break;
 		case 2:
 			// know what's next
-			title = "Know what's next";
-			leftIndex  = 1;
-			rightIndex = 3;
+			title       = "Know what's next";
+			leftIndex   = 1;
+			rightIndex  = 3;
+			imageSource = "next.gif";
+			imageAlt    = "Know what's next";
 			break;
 		case 3:
 			// free up your attention
-			title = "Free up your attention";
-			leftIndex  = 2;
-			rightIndex = 0;
+			title       = "Free up your attention";
+			leftIndex   = 2;
+			rightIndex  = 0;
+			imageSource = "freeattention.gif";
+			imageAlt    = "Free up your attention";
 			break;
 		default:
 			break;
@@ -67,9 +84,13 @@ function updateDemoDivOnIndex(index) {
 
 	var counter = index + 1;
 
+	imageSource = `/assets/gifs/${imageSource}`;
+
 	demoDisplay.find(".title").text(title);
 	demoDisplay.find(".index-counter").text(counter);
 	demoDisplay.find(".button.left").data("index", leftIndex);
 	demoDisplay.find(".button.right").data("index", rightIndex);
+	demoDisplay.find(".gif").attr("src", imageSource);
+	demoDisplay.find(".gif").attr("alt", imageAlt);
 
 }
