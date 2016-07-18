feb2c786637611c55073c0190e73878addb9f778
kevinsuh <kevinsuh34@gmail.com>
2016-07-17 22:52:54 -0400

    Demo display on splash page

--- build/public/js/splash.js
+++ build/public/js/splash.js
@@ -13,21 +13,29 @@ $(document).ready(function () {
 		var demoDisplay = $("#demo-display");
 		demoDisplay.fadeIn(300);
 
+		var title = "Demo";
 		switch (index) {
 			case 0:
 				// clear objectives, every day
+				title = "Clear objectives, every day";
 				break;
 			case 1:
 				// empower your self-awareness
+				title = "Empower your self-awareness";
 				break;
 			case 2:
 				// know what's next
+				title = "Know what's next";
 				break;
 			case 3:
 				// free up your attention
+				title = "Free up your attention";
 				break;
-				alert($(".benefit-box").index(this));
+			default:
+				break;
 		}
+
+		demoDisplay.find(".title").text(title);
 	});
 
 	$("#demo-display .circle").click(function () {
