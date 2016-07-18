8dc5832fcd595ce1b5a28594c45619a73ee956c4
kevinsuh <kevinsuh34@gmail.com>
2016-07-17 21:35:58 -0400

    Add in jQuery `splash.js` and put in benefit boxes

--- src/public/js/splash.js
+++ src/public/js/splash.js
@@ -1,5 +1,21 @@
+/**
+ * 	jQuery functionality for splash page!
+ */
+
 $(document).ready(function(){
-	$(".body-container").click(function(){
-		$(this).hide();
-	});
+
+	var orange = `#FFA819`;
+	var blue   = `#3F76FF`;
+	
+	// hover to change color functionality
+	// $(".benefit-box").mouseenter(() => {
+	// 	$(this).animate({
+	// 		color: orange
+	// 	}, 500);
+	// }).mouseout(() => {
+	// 	$(this).animate({
+	// 		color: blue
+	// 	}, 500);
+	// })
+
 });
