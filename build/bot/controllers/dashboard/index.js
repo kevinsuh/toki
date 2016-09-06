e29eeb698493bb64c263d20c0360f7ad7c7b11a6
kevinsuh <kevinsuh34@gmail.com>
2016-09-06 17:21:56 -0400

    Update dashboard appropriately on `/invite`

--- build/bot/controllers/dashboard/index.js
+++ build/bot/controllers/dashboard/index.js
@@ -83,341 +83,67 @@ exports.default = function (controller) {
 				}
 			});
 		});
-
-		/*
-  // CHANNEL
-  yo joined the channel or group:
-  { type: 'channel_joined',
-  channel:
-   { id: 'C28K3L3K6',
-     name: 'test-dashboard',
-     is_channel: true,
-     created: 1473168840,
-     creator: 'U121ZK15J',
-     is_archived: false,
-     is_general: false,
-     is_member: true,
-     last_read: '1473168840.000004',
-     latest:
-      { user: 'U1NCGAETZ',
-        inviter: 'U121ZK15J',
-        text: '<@U1NCGAETZ|test> has joined the channel',
-        type: 'message',
-        subtype: 'channel_join',
-        ts: '1473168840.000004' },
-     unread_count: 0,
-     unread_count_display: 0,
-     members: [ 'U121ZK15J', 'U1J649CA0', 'U1NCGAETZ' ],
-     topic: { value: '', creator: '', last_set: 0 },
-     purpose:
-      { value: 'test toki’s dashboard',
-        creator: 'U121ZK15J',
-        last_set: 1473168841 } },
-  intentObject: { _text: 'channel_joined', entities: { intent: false } } }
-  // GROUP
-  { type: 'group_joined',
-  channel:
-   { id: 'G285J09KP',
-     name: 'testchannel',
-     is_group: true,
-     created: 1473025890,
-     creator: 'U121U9CAU',
-     is_archived: false,
-     is_mpim: false,
-     is_open: true,
-     last_read: '1473168248.000031',
-     latest:
-      { user: 'U1J649CA0',
-        text: '<@U1J649CA0|dev_navi> has left the group',
-        type: 'message',
-        subtype: 'group_leave',
-        ts: '1473168248.000031' },
-     unread_count: 0,
-     unread_count_display: 0,
-     members: [ 'U121U9CAU', 'U121ZK15J', 'U1F8T3HB6', 'U1J649CA0', 'U263PHCLQ' ],
-     topic: { value: '', creator: 'U121U9CAU', last_set: 1473025890 },
-     purpose: { value: '', creator: 'U121U9CAU', last_set: 1473025890 } },
-  intentObject: { _text: 'group_joined', entities: { intent: false } } }
-  	 */
 	});
 
 	controller.on('setup_dashboard_flow', function (bot, config) {
-
-		console.log('\n\n ~~ setting up dashboard now ~~ \n\n');
-		console.log(config);
-
 		var ChannelId = config.ChannelId;
 		var BotSlackUserId = config.BotSlackUserId;
 		var tz = config.tz;
 
-		// 1. find ChannelId using Slack API
-		// 2. get members of that channel
-		// 3. make sure Toki is in the channel
-		// 4. if so, post in it with the dashboard!
+		// introduction message
 
-		bot.api.channels.info({
-			channel: ChannelId
-		}, function (err, response) {
+		bot.send({
+			channel: ChannelId,
+			text: 'Hi! I\'m Toki, your team\'s sidekick to make the most of your attention each day :raised_hands:\nI\'ll set up a dashboard here of your team\'s statuses each day. If you ever need a refresher on how I work, just say `/explain` and I\'d love to go into more detail!'
+		}, function () {
 
-			if (!err) {
-				(function () {
-
-					console.log('\n\n\n successfully got channel in setup_dashboard_flow');
-					console.log(response);
-
-					var channel = response.channel;
-					var _response$channel = response.channel;
-					var id = _response$channel.id;
-					var name = _response$channel.name;
-					var members = _response$channel.members;
+			// delete all teamPulseMessages for now and then create new one
+			// when inviting for the first time
+			_models2.default.Channel.find({
+				where: { ChannelId: ChannelId }
+			}).then(function (channel) {
+				var ChannelId = channel.ChannelId;
+				var tz = channel.tz;
+				var TeamId = channel.TeamId;
 
 
-					_models2.default.Channel.find({
-						where: { ChannelId: id }
-					}).then(function (channel) {
-						var tz = channel.tz;
-						var ChannelId = channel.ChannelId;
+				_models2.default.Team.find({
+					where: ['"Team"."TeamId" = ?', TeamId]
+				}).then(function (team) {
+					var accessToken = team.accessToken;
 
+					if (!accessToken) {
+						console.log('\n\n\n ERROR... NO ACCESS TOKEN FOR BOT: ' + accessToken);
+						return;
+					}
 
-						if (!tz) {
-							console.log('\n\n\n channel needs tz... \n\n\n');
-							return;
-						}
+					bot.api.channels.history({
+						token: accessToken,
+						channel: ChannelId
+					}, function (err, response) {
+						var messages = response.messages;
 
-						// introduction message
-						bot.send({
-							channel: ChannelId,
-							text: 'Hi! I\'m Toki, your team\'s sidekick to make the most of your attention each day :raised_hands:\nI\'ll set up a dashboard here of your team\'s statuses each day. If you ever need a refresher on how I work, just say `/explain` and I\'d love to go into more detail!'
-						});
+						messages.forEach(function (message) {
 
-						var zoneAbbrString = (0, _momentTimezone2.default)().tz(tz).zoneAbbr(); // ex. EDT
-						var todayString = (0, _momentTimezone2.default)().tz(tz).format('MMMM Do YYYY'); // ex. September 6th, 2016
+							// user is `SlackUserId`
+							var user = message.user;
+							var attachments = message.attachments;
+							var ts = message.ts;
 
-						var text = ':raised_hands: *Team Pulse for ' + todayString + '* :raised_hands:';
-						var attachments = [];
+							// find the message of the team pulse
 
-						var dashboardMemberSlackUserIds = [];
-						members.forEach(function (MemberSlackUserId) {
-
-							if (MemberSlackUserId != BotSlackUserId) {
-								dashboardMemberSlackUserIds.push(MemberSlackUserId);
+							if (user == BotSlackUserId && attachments && attachments[0].callback_id == _constants.constants.dashboardCallBackId) {
+								bot.api.chat.delete({
+									ts: ts,
+									channel: ChannelId
+								});
 							}
 						});
 
-						_models2.default.User.findAll({
-							where: ['"User"."SlackUserId" IN (?)', dashboardMemberSlackUserIds]
-						}).then(function (users) {
-
-							var sessionPromises = [];
-							var dashboardUsers = {}; // by SlackUserId key i.e. dashboardUsers[`UI14242`] = {}
-
-							users.forEach(function (user) {
-
-								sessionPromises.push(_models2.default.Session.find({
-									where: {
-										UserId: user.dataValues.id,
-										live: true,
-										open: true
-									},
-									include: [_models2.default.User]
-								}));
-								dashboardUsers[user.dataValues.SlackUserId] = {
-									session: false,
-									user: user
-								};
-							});
-
-							var userSessions = []; // unique sessions only
-							Promise.all(sessionPromises).then(function (userSessions) {
-
-								userSessions.forEach(function (userSession) {
-
-									if (userSession && dashboardUsers[userSession.dataValues.User.SlackUserId]) {
-										dashboardUsers[userSession.dataValues.User.SlackUserId].session = userSession;
-									}
-								});
-
-								// you will have all the sessions and users here
-								// now you must post the dashboard with the session info
-								// via attachment that has specific msg
-
-								console.log('\n\n\n dashboardUsers object: ');
-								console.log(dashboardUsers);
-
-								/*
-         dashboardUsers object:
-        { U1NCGAETZ: { session: false },
-          U121ZK15J:
-           { session:
-              { dataValues: [Object],
-                _previousDataValues: [Object],
-                _changed: {},
-                '$modelOptions': [Object],
-                '$options': [Object],
-                hasPrimaryKeys: true,
-                __eagerlyLoadedAssociations: [],
-                isNewRecord: false,
-                User: [Object] } } }
-         */
-								attachments = [{
-									mrkdwn_in: ["text", "fields"],
-									callback_id: _constants.constants.dashboardCallBackId,
-									fallback: 'Here\'s your team pulse!',
-									fields: [{
-										title: "Current Priority",
-										short: true
-									}, {
-										title: 'Until (' + zoneAbbrString + ')',
-										short: true
-									}],
-									color: _constants.colorsHash.white.hex
-								}];
-
-								// iterate through dashboardUsers and put into alphabetized array
-								var dashboardUsersArrayAlphabetical = [];
-								_lodash2.default.forOwn(dashboardUsers, function (value, key) {
-
-									// value is the object that has value.user and value.session
-									dashboardUsersArrayAlphabetical.push(value);
-								});
-
-								dashboardUsersArrayAlphabetical.sort(function (a, b) {
-
-									var nameA = a.user.dataValues.SlackName;
-									var nameB = b.user.dataValues.SlackName;
-									return nameA < nameB ? -1 : nameA > nameB ? 1 : 0;
-								});
-
-								dashboardUsersArrayAlphabetical.forEach(function (dashboardUser) {
-
-									console.log(dashboardUser);
-
-									var session = dashboardUser.session;
-									var SlackUserId = dashboardUser.user.dataValues.SlackUserId;
-
-
-									var sessionContent = void 0;
-									var sessionTime = void 0;
-									var sessionColor = void 0;
-
-									if (session) {
-										sessionContent = '`' + session.dataValues.content + '`';
-										sessionTime = (0, _momentTimezone2.default)(session.dataValues.endTime).tz(tz).format("h:mma");
-										sessionColor = _constants.colorsHash.toki_purple.hex;
-									} else {
-										sessionContent = '_No active priority_';
-										sessionTime = '';
-										sessionColor = _constants.colorsHash.grey.hex;
-									}
-
-									// alphabetize the 
-									attachments.push({
-										attachment_type: 'default',
-										callback_id: "DASHBOARD_SESSION_INFO_FOR_USER",
-										fallback: 'Here\'s the session info!',
-										text: '<@' + SlackUserId + '>',
-										mrkdwn_in: ["text", "fields"],
-										fields: [{
-											value: sessionContent,
-											short: true
-										}, {
-											value: sessionTime,
-											short: true
-										}],
-										color: sessionColor,
-										actions: [{
-											name: "SEND_PING",
-											text: "Send Message",
-											value: '{"pingUser": true, "PingToSlackUserId": "' + SlackUserId + '"}',
-											type: "button"
-										}]
-									});
-								});
-
-								attachments.push({
-									attachment_type: 'default',
-									callback_id: "DASHBOARD_ACTIONS_FOR_USER",
-									fallback: 'Would you like to set a priority?',
-									mrkdwn_in: ["text", "fields"],
-									color: _constants.colorsHash.toki_yellow.hex,
-									text: "_Update your current priority_",
-									actions: [{
-										name: "SET_PRIORITY",
-										text: "Set My Priority",
-										value: '{"setPriority": true}',
-										type: "button"
-									}]
-								});
-
-								/*
-        
-        // this is the attachments that is about to send
-        [ { mrkdwn_in: [ 'text', 'fields' ],
-            fields: [ [Object], [Object] ],
-            color: '#ffffff' },
-          { attachment_type: 'default',
-            callback_id: 'DASHBOARD_SESSION_INFO_FOR_USER',
-            fallback: 'Here\'s the session info!',
-            text: '<@U121ZK15J>',
-            mrkdwn_in: [ 'text', 'fields' ],
-            fields: [ [Object], [Object] ],
-            color: '#C1C1C3',
-            actions: [ [Object] ] },
-          { attachment_type: 'default',
-            callback_id: 'DASHBOARD_SESSION_INFO_FOR_USER',
-            fallback: 'Here\'s the session info!',
-            text: '<@U1NCGAETZ>',
-            mrkdwn_in: [ 'text', 'fields' ],
-            fields: [ [Object], [Object] ],
-            color: '#C1C1C3',
-            actions: [ [Object] ] } ]
-         */
-
-								// send the message!
-								bot.send({
-									channel: ChannelId,
-									text: text,
-									attachments: attachments
-								});
-							});
-						});
+						(0, _slackHelpers.updateDashboardForChannelId)(bot, ChannelId);
 					});
-
-					/*
-     { ok: true,
-       channel:
-        { id: 'C28K3L3K6',
-          name: 'test-dashboard',
-          is_channel: true,
-          created: 1473168840,
-          creator: 'U121ZK15J',
-          is_archived: false,
-          is_general: false,
-          is_member: true,
-          last_read: '1473175401.000034',
-          latest:
-           { user: 'U1J649CA0',
-             inviter: 'U121ZK15J',
-             text: '<@U1J649CA0|dev_navi> has joined the channel',
-             type: 'message',
-             subtype: 'channel_join',
-             ts: '1473175404.000035' },
-          unread_count: 1,
-          unread_count_display: 0,
-          members: [ 'U121ZK15J', 'U1J649CA0', 'U1NCGAETZ' ],
-          topic: { value: '', creator: '', last_set: 0 },
-          purpose:
-           { value: 'test toki’s dashboard',
-             creator: 'U121ZK15J',
-             last_set: 1473168841 } } }
-     */
-				})();
-			} else {
-
-				console.log('\n\n\n error in getting channel info in setup_dashboard_flow');
-				console.log(err);
-				console.log('\n\n\n');
-			}
+				});
+			});
 		});
 	});
 };
@@ -445,6 +171,8 @@ var _constants = require('../../lib/constants');
 var _messageHelpers = require('../../lib/messageHelpers');
 
 var _sessions = require('../sessions');
+
+var _slackHelpers = require('../../lib/slackHelpers');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
--- src/bot/controllers/dashboard/index.js
+++ src/bot/controllers/dashboard/index.js
@@ -7,6 +7,7 @@ import dotenv from 'dotenv';
 import { utterances, colorsArray, constants, buttonValues, colorsHash, timeZones, timeZoneAttachments } from '../../lib/constants';
 import { witTimeResponseToTimeZoneObject, convertMinutesToHoursString, getUniqueSlackUsersFromString, getStartSessionOptionsAttachment, commaSeparateOutStringArray } from '../../lib/messageHelpers';
 import { notInSessionWouldYouLikeToStartOne } from '../sessions';
+import { updateDashboardForChannelId } from '../../lib/slackHelpers';
 
 export default function(controller) {
 
@@ -57,7 +58,7 @@ export default function(controller) {
 					controller.trigger(`setup_dashboard_flow`, [ bot, config ]);
 
 				} else {
-					
+
 					// get timezone for the channel
 					bot.startPrivateConversation({ user: creator }, (err, convo) => {
 
@@ -87,358 +88,64 @@ export default function(controller) {
 
 		})
 
-		/*
-// CHANNEL
- yo joined the channel or group:
-{ type: 'channel_joined',
-  channel:
-   { id: 'C28K3L3K6',
-     name: 'test-dashboard',
-     is_channel: true,
-     created: 1473168840,
-     creator: 'U121ZK15J',
-     is_archived: false,
-     is_general: false,
-     is_member: true,
-     last_read: '1473168840.000004',
-     latest:
-      { user: 'U1NCGAETZ',
-        inviter: 'U121ZK15J',
-        text: '<@U1NCGAETZ|test> has joined the channel',
-        type: 'message',
-        subtype: 'channel_join',
-        ts: '1473168840.000004' },
-     unread_count: 0,
-     unread_count_display: 0,
-     members: [ 'U121ZK15J', 'U1J649CA0', 'U1NCGAETZ' ],
-     topic: { value: '', creator: '', last_set: 0 },
-     purpose:
-      { value: 'test toki’s dashboard',
-        creator: 'U121ZK15J',
-        last_set: 1473168841 } },
-  intentObject: { _text: 'channel_joined', entities: { intent: false } } }
-
-// GROUP
-{ type: 'group_joined',
-  channel:
-   { id: 'G285J09KP',
-     name: 'testchannel',
-     is_group: true,
-     created: 1473025890,
-     creator: 'U121U9CAU',
-     is_archived: false,
-     is_mpim: false,
-     is_open: true,
-     last_read: '1473168248.000031',
-     latest:
-      { user: 'U1J649CA0',
-        text: '<@U1J649CA0|dev_navi> has left the group',
-        type: 'message',
-        subtype: 'group_leave',
-        ts: '1473168248.000031' },
-     unread_count: 0,
-     unread_count_display: 0,
-     members: [ 'U121U9CAU', 'U121ZK15J', 'U1F8T3HB6', 'U1J649CA0', 'U263PHCLQ' ],
-     topic: { value: '', creator: 'U121U9CAU', last_set: 1473025890 },
-     purpose: { value: '', creator: 'U121U9CAU', last_set: 1473025890 } },
-  intentObject: { _text: 'group_joined', entities: { intent: false } } }
-
-		 */
-
-
 	});
 
 	controller.on(`setup_dashboard_flow`, (bot, config) => {
 
-		console.log(`\n\n ~~ setting up dashboard now ~~ \n\n`);
-		console.log(config);
-
 		const { ChannelId, BotSlackUserId, tz } = config;
 
-		// 1. find ChannelId using Slack API
-		// 2. get members of that channel
-		// 3. make sure Toki is in the channel
-		// 4. if so, post in it with the dashboard!
-		
-		bot.api.channels.info({
-			channel: ChannelId
-		}, (err, response) => {
+		// introduction message
+		bot.send({
+			channel: ChannelId,
+			text: `Hi! I'm Toki, your team's sidekick to make the most of your attention each day :raised_hands:\nI'll set up a dashboard here of your team's statuses each day. If you ever need a refresher on how I work, just say \`/explain\` and I'd love to go into more detail!`
+		}, () => {
 
-			if (!err) {
+			// delete all teamPulseMessages for now and then create new one
+			// when inviting for the first time
+			models.Channel.find({
+				where: { ChannelId }
+			})
+			.then((channel) => {
 
-				console.log(`\n\n\n successfully got channel in setup_dashboard_flow`);
-				console.log(response);
+				const { ChannelId, tz, TeamId } = channel;
 
-				const { channel, channel: { id, name, members } } = response;
-
-				models.Channel.find({
-					where: { ChannelId: id }
+				models.Team.find({
+					where: [`"Team"."TeamId" = ?`, TeamId]
 				})
-				.then((channel) => {
+				.then((team) => {
 
-					const { tz, ChannelId } = channel;
-
-					if (!tz) {
-						console.log(`\n\n\n channel needs tz... \n\n\n`);
+					const { accessToken } = team;
+					if (!accessToken) {
+						console.log(`\n\n\n ERROR... NO ACCESS TOKEN FOR BOT: ${accessToken}`);
 						return;
 					}
 
-					// introduction message
-					bot.send({
-						channel: ChannelId,
-						text: `Hi! I'm Toki, your team's sidekick to make the most of your attention each day :raised_hands:\nI'll set up a dashboard here of your team's statuses each day. If you ever need a refresher on how I work, just say \`/explain\` and I'd love to go into more detail!`
-					});
+					bot.api.channels.history({
+						token: accessToken,
+						channel: ChannelId
+					}, (err, response) => {
 
-					let zoneAbbrString = moment().tz(tz).zoneAbbr(); // ex. EDT
-					let todayString    = moment().tz(tz).format(`MMMM Do YYYY`); // ex. September 6th, 2016
+						const { messages } = response;
+						messages.forEach((message) => {
 
-					let text        = `:raised_hands: *Team Pulse for ${todayString}* :raised_hands:`;
-					let attachments = [];
+							// user is `SlackUserId`
+							const { user, attachments, ts } = message;
 
-					let dashboardMemberSlackUserIds = [];
-					members.forEach((MemberSlackUserId) => {
-
-						if (MemberSlackUserId != BotSlackUserId) {
-							dashboardMemberSlackUserIds.push(MemberSlackUserId);
-						}
-
-					});
-
-					models.User.findAll({
-						where: [ `"User"."SlackUserId" IN (?)`, dashboardMemberSlackUserIds ]
-					})
-					.then((users) => {
-
-						let sessionPromises = [];
-						let dashboardUsers  = {}; // by SlackUserId key i.e. dashboardUsers[`UI14242`] = {}
-
-						users.forEach((user) => {
-
-							sessionPromises.push(models.Session.find({
-								where: {
-									UserId: user.dataValues.id,
-									live: true,
-									open: true
-								},
-								include: [ models.User ]
-							}));
-							dashboardUsers[user.dataValues.SlackUserId] = {
-								session: false,
-								user: user
-							};
+							// find the message of the team pulse
+							if (user == BotSlackUserId && attachments && attachments[0].callback_id == constants.dashboardCallBackId) {
+								bot.api.chat.delete({
+									ts,
+									channel: ChannelId
+								});
+							}
 
 						});
 
-						let userSessions = []; // unique sessions only
-						Promise.all(sessionPromises)
-						.then((userSessions) => {
-
-							userSessions.forEach((userSession) => {
-
-								if (userSession && dashboardUsers[userSession.dataValues.User.SlackUserId]) {
-									dashboardUsers[userSession.dataValues.User.SlackUserId].session = userSession;
-								}
-
-							});
-
-							// you will have all the sessions and users here
-							// now you must post the dashboard with the session info
-							// via attachment that has specific msg
-
-							console.log(`\n\n\n dashboardUsers object: `);
-							console.log(dashboardUsers);
-
-							/*
-							 dashboardUsers object:
-							{ U1NCGAETZ: { session: false },
-							  U121ZK15J:
-							   { session:
-							      { dataValues: [Object],
-							        _previousDataValues: [Object],
-							        _changed: {},
-							        '$modelOptions': [Object],
-							        '$options': [Object],
-							        hasPrimaryKeys: true,
-							        __eagerlyLoadedAssociations: [],
-							        isNewRecord: false,
-							        User: [Object] } } }
-							 */
-							attachments = [{
-								mrkdwn_in: [ "text", "fields" ],
-								callback_id: constants.dashboardCallBackId,
-								fallback: `Here's your team pulse!`,
-								fields: [
-									{
-										title: "Current Priority",
-										short: true
-									},
-									{
-										title: `Until (${zoneAbbrString})`,
-										short: true
-									}
-								],
-								color: colorsHash.white.hex
-							}];
-
-							// iterate through dashboardUsers and put into alphabetized array
-							let dashboardUsersArrayAlphabetical = [];
-							_.forOwn(dashboardUsers, (value, key) => {
-
-								// value is the object that has value.user and value.session
-								dashboardUsersArrayAlphabetical.push(value);
-
-							});
-
-							dashboardUsersArrayAlphabetical.sort((a, b) => {
-								
-								let nameA = a.user.dataValues.SlackName;
-								let nameB = b.user.dataValues.SlackName;
-								return (nameA < nameB) ? -1 : (nameA > nameB) ? 1 : 0;
-
-							});
-
-							dashboardUsersArrayAlphabetical.forEach((dashboardUser) => {
-
-								console.log(dashboardUser);
-
-								const { session, user: { dataValues: { SlackUserId } } } = dashboardUser;
-
-								let sessionContent;
-								let sessionTime;
-								let sessionColor;
-
-								if (session) {
-									sessionContent = `\`${session.dataValues.content}\``;
-									sessionTime    = moment(session.dataValues.endTime).tz(tz).format("h:mma");
-									sessionColor   = colorsHash.toki_purple.hex;
-								} else {
-									sessionContent = `_No active priority_`;
-									sessionTime    = ``;
-									sessionColor   = colorsHash.grey.hex;
-								}
-
-								// alphabetize the 
-								attachments.push({
-									attachment_type: 'default',
-									callback_id: "DASHBOARD_SESSION_INFO_FOR_USER",
-									fallback: `Here's the session info!`,
-									text: `<@${SlackUserId}>`,
-									mrkdwn_in: [ "text", "fields" ],
-									fields: [
-										{
-											value: sessionContent,
-											short: true
-										},
-										{
-											value: sessionTime,
-											short: true
-										}
-									],
-									color: sessionColor,
-									actions: [
-										{
-											name: "SEND_PING",
-											text: "Send Message",
-											value: `{"pingUser": true, "PingToSlackUserId": "${SlackUserId}"}`,
-											type: "button"
-										}
-									]
-								});
-
-							});
-
-							attachments.push({
-								attachment_type: 'default',
-								callback_id: "DASHBOARD_ACTIONS_FOR_USER",
-								fallback: `Would you like to set a priority?`,
-								mrkdwn_in: [ "text", "fields" ],
-								color: colorsHash.toki_yellow.hex,
-								text: "_Update your current priority_",
-								actions: [
-									{
-										name: "SET_PRIORITY",
-										text: "Set My Priority",
-										value: `{"setPriority": true}`,
-										type: "button"
-									}
-								]
-							});
-
-/*
-
-// this is the attachments that is about to send
-[ { mrkdwn_in: [ 'text', 'fields' ],
-    fields: [ [Object], [Object] ],
-    color: '#ffffff' },
-  { attachment_type: 'default',
-    callback_id: 'DASHBOARD_SESSION_INFO_FOR_USER',
-    fallback: 'Here\'s the session info!',
-    text: '<@U121ZK15J>',
-    mrkdwn_in: [ 'text', 'fields' ],
-    fields: [ [Object], [Object] ],
-    color: '#C1C1C3',
-    actions: [ [Object] ] },
-  { attachment_type: 'default',
-    callback_id: 'DASHBOARD_SESSION_INFO_FOR_USER',
-    fallback: 'Here\'s the session info!',
-    text: '<@U1NCGAETZ>',
-    mrkdwn_in: [ 'text', 'fields' ],
-    fields: [ [Object], [Object] ],
-    color: '#C1C1C3',
-    actions: [ [Object] ] } ]
- */
-
-
-							// send the message!
-							bot.send({
-								channel: ChannelId,
-								text,
-								attachments
-							})
-
-						})
+						updateDashboardForChannelId(bot, ChannelId);
 
 					});
-
-				})
-
-/*
-{ ok: true,
-  channel:
-   { id: 'C28K3L3K6',
-     name: 'test-dashboard',
-     is_channel: true,
-     created: 1473168840,
-     creator: 'U121ZK15J',
-     is_archived: false,
-     is_general: false,
-     is_member: true,
-     last_read: '1473175401.000034',
-     latest:
-      { user: 'U1J649CA0',
-        inviter: 'U121ZK15J',
-        text: '<@U1J649CA0|dev_navi> has joined the channel',
-        type: 'message',
-        subtype: 'channel_join',
-        ts: '1473175404.000035' },
-     unread_count: 1,
-     unread_count_display: 0,
-     members: [ 'U121ZK15J', 'U1J649CA0', 'U1NCGAETZ' ],
-     topic: { value: '', creator: '', last_set: 0 },
-     purpose:
-      { value: 'test toki’s dashboard',
-        creator: 'U121ZK15J',
-        last_set: 1473168841 } } }
-*/
-
-			} else {
-
-				console.log(`\n\n\n error in getting channel info in setup_dashboard_flow`);
-				console.log(err);
-				console.log(`\n\n\n`);
-
-			}
-
+				});
+			});
 		});
 
 	});
